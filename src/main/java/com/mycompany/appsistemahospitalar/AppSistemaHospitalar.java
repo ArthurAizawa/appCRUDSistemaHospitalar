d009f298cbb1eb10cec3197ea8c0a5eae00c5967
Arthur Aizawa <arthuraizawaspg@gmail.com>
2026-07-22 11:12:43 -0300

    create project

---
+++ src/main/java/com/mycompany/appsistemahospitalar/AppSistemaHospitalar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.appsistemahospitalar;
+
+/**
+ *
+ * @author arthu
+ */
+public class AppSistemaHospitalar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
