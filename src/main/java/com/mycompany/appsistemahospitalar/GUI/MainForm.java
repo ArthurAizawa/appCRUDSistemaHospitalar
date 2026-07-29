ac8ee622604217ae42f64be62274a288ddd0c0aa
Arthur Aizawa <arthuraizawaspg@gmail.com>
2026-07-28 22:51:06 -0300

    validação MedicoAtualizar

--- src/main/java/com/mycompany/appsistemahospitalar/GUI/MainForm.java
+++ src/main/java/com/mycompany/appsistemahospitalar/GUI/MainForm.java
@@ -4,6 +4,8 @@
  */
 package com.mycompany.appsistemahospitalar.GUI;
 
+import com.mycompany.appsistemahospitalar.entities.Especialidade;
+
 /**
  *
  * @author arthu
@@ -211,7 +213,6 @@ public class MainForm extends javax.swing.JFrame {
     }//GEN-LAST:event_sairMenuItem1ActionPerformed
 
     private void pacienteCadastroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_pacienteCadastroActionPerformed
-
         CadastroPaciente form = new CadastroPaciente();
         
         desktopPane.add(form);
@@ -219,9 +220,7 @@ public class MainForm extends javax.swing.JFrame {
     }//GEN-LAST:event_pacienteCadastroActionPerformed
 
     private void agendamentoCadastroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_agendamentoCadastroActionPerformed
-        CadastroAgendamento novoagen = new CadastroAgendamento();
-        desktopPane.add(novoagen);
-        novoagen.setVisible(true);
+        // TODO add your handling code here:
     }//GEN-LAST:event_agendamentoCadastroActionPerformed
 
     private void medicoConsultaMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_medicoConsultaMenuItemActionPerformed
@@ -237,10 +236,10 @@ public class MainForm extends javax.swing.JFrame {
     }//GEN-LAST:event_agendamentoConsultaMenuItem1ActionPerformed
 
     private void especialidadeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_especialidadeActionPerformed
-       
-        CadastroEspecialidade tela = new CadastroEspecialidade();
-        desktopPane.add(tela);
-        tela.setVisible(true);
+        CadastroEspecialidade form = new CadastroEspecialidade();
+        desktopPane.add(form);
+        form.setVisible(true);
+        
     }//GEN-LAST:event_especialidadeActionPerformed
 
     private void medicoAtualizarMenuActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_medicoAtualizarMenuActionPerformed
